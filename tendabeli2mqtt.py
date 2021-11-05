358e5faa0472d24e6058372e8145f2701fb2f673
Tediore <tediore.maliwan@gmail.com>
2021-11-04 20:22:03 -0500

    ready for initial release

--- tendabeli2mqtt.py
+++ tendabeli2mqtt.py
@@ -13,11 +13,140 @@ MQTT_USER = os.getenv('MQTT_USER')
 MQTT_PASSWORD = os.getenv('MQTT_PASSWORD')
 MQTT_QOS = int(os.getenv('MQTT_QOS', 1))
 BASE_TOPIC = os.getenv('BASE_TOPIC', 'tendabeli2mqtt')
-HOME_ASSISTANT = os.getenv('HOME_ASSISTANT', True)
+# HOME_ASSISTANT = os.getenv('HOME_ASSISTANT', True)
 DEVICE_IPS = os.getenv('DEVICE_IPS')
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
 
+HOME_ASSISTANT = True
+
 client = mqtt_client.Client(BASE_TOPIC)
+
+class Devices:
+    def __init__(self):
+        self.device_ips = DEVICE_IPS.split(',')
+        self.devices = {}
+        self.timeout = 3
+        self.states = ['off', 'on']
+
+    def get_device_info(self):
+        """Get information about Tenda Beli device(s)"""
+        for ip in self.device_ips:
+            try:
+                url = requests.get(f'http://{ip}:5000/getDetail', timeout=self.timeout)
+                info = json.loads(url.text)
+                mac = info['data']['mac'].replace(':','')
+                self.devices[mac] = {}
+                self.devices[mac]['ip_addr'] = ip
+                self.devices[mac]['retry_count'] = 0
+                for item in info['data']:
+                    self.devices[mac][item] = info['data'][item]
+
+            except Exception as e:
+                logging.warning(f'Tenda device not found at {ip}; ignoring.')
+        
+    def set_state(self, device_mac, state):
+        try:
+            requests.post(f'http://{self.devices[device_mac]["ip_addr"]}:5000/setSta', data=json.dumps({"status":state}), timeout=self.timeout, headers={'content-type': 'application/json'})
+
+        except Exception as e:
+            logging.error(f'Set state for Tenda device at {self.devices[device_mac]["ip_addr"]} failed: {e}')
+
+        d.get_state(device_mac)
+
+    def get_state(self, device_mac):
+        base_url = f'http://{self.devices[device_mac]["ip_addr"]}:5000'
+        try:
+            state_url = requests.get(f'{base_url}/getSta', timeout=self.timeout)
+            rssi_url = requests.get(f'{base_url}/getDetail', timeout=self.timeout)
+            state_text = json.loads(state_url.text)
+            rssi_text = json.loads(rssi_url.text)
+            state = state_text['data']['status']
+            rssi = rssi_text['data']['rssi']
+            ip_addr = self.devices[device_mac]["ip_addr"]
+            if self.devices[device_mac]['retry_count'] > 0:
+                logging.info(f'Reconnected to Tenda device at {ip_addr}')
+                self.devices[device_mac]['retry_count'] = 0
+            client.publish(f'{BASE_TOPIC}/{device_mac}/state', self.states[state], MQTT_QOS)
+            client.publish(f'{BASE_TOPIC}/{device_mac}/rssi', rssi, MQTT_QOS)
+
+        except Exception as e:
+            if self.devices[device_mac]['retry_count'] < 3:
+                logging.error(f'Unable to communicate with Tenda device at {ip_addr}: {e}')
+            self.devices[device_mac]['retry_count'] += 1
+            if self.devices[device_mac]['retry_count'] > 2:
+                client.publish(f'{BASE_TOPIC}/{device_mac}/status', 'offline', MQTT_QOS, True)
+                if self.devices[device_mac]['retry_count'] < 4:
+                    logging.warning('Tenda device unreachable; marking as offline.')
+                    logging.warning('Trying to reconnect every 10 sec in the background...')
+
+        if self.devices[device_mac]['retry_count'] < 1:
+            return state
+
+    def poll_device(self):
+        """Poll for Tenda device information every 10 seconds in case button is pressed on device"""
+        while True:
+            sleep(10)
+            for device in d.devices:
+                d.get_state(device)
+
+    def mqtt_discovery(self):
+        """Send Home Assistant MQTT discovery payloads"""
+        if HOME_ASSISTANT:
+            ha_switch = 'homeassistant/switch/'
+            ha_sensor = 'homeassistant/sensor/'
+            try:
+                for device_mac in self.devices:
+                    device_info = self.devices[device_mac]
+                    name = device_info['nick']
+                    mac = device_info['mac'].replace(':','')
+                    serial_no = device_info['sn']
+                    model = device_info['model']
+                    fw_ver = device_info['sft_ver']
+
+                    # switch payload
+                    client.publish(f'{ha_switch}{device_mac}/config',json.dumps({
+                        'availability': [
+                            {'topic': f'{BASE_TOPIC}/status'},
+                            {'topic': f'{BASE_TOPIC}/{device_mac}/status'}
+                            ], 
+                        'name': f'{name} switch', 
+                        'command_topic': f'{BASE_TOPIC}/set_state/{mac}', 
+                        'state_topic': f'{BASE_TOPIC}/{mac}/state', 
+                        'payload_on': 'on', 
+                        'payload_off': 'off', 
+                        'unique_id': f'{mac}', 
+                        'device': {
+                            'name': f'{name}', 
+                            'identifiers': f'{serial_no}', 
+                            'manufacturer': 'Tenda', 
+                            'model': f'{model}', 
+                            'sw_version': f'{fw_ver}'}, 
+                            'platform': 'mqtt', 
+                            'icon': 'mdi:switch'
+                            }), 1, True)
+
+                    # RSSI sensor payload
+                    client.publish(f'{ha_sensor}{device_mac}/config',json.dumps({
+                        'availability': [
+                            {'topic': f'{BASE_TOPIC}/status'},
+                            {'topic': f'{BASE_TOPIC}/{device_mac}/status'}
+                            ], 
+                        'name': f'{name} switch RSSI', 
+                        'state_topic': f'{BASE_TOPIC}/{mac}/rssi', 
+                        'unique_id': f'{mac}', 
+                        'device': {
+                            'name': f'{name}', 
+                            'identifiers': f'{serial_no}', 
+                            'manufacturer': 'Tenda', 
+                            'model': f'{model}', 
+                            'sw_version': f'{fw_ver}'}, 
+                            'platform': 'mqtt', 
+                            'icon': 'mdi:signal-variant',
+                            'unit_of_measurement': 'dBm'
+                            }), 1, True)
+
+            except Exception as e:
+                logging.error(f'Unable to publish Home Assistant MQTT discovery payloads: {e}')
 
 def mqtt_connect():
     """Connect to MQTT broker and set LWT"""
@@ -38,7 +167,7 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe(f'{BASE_TOPIC}/#')
-    # mqtt_discovery()
+    d.mqtt_discovery()
 
 def on_message(client, userdata, msg):
     """Listen for MQTT payloads and forward to Tenda device"""
@@ -50,43 +179,11 @@ def on_message(client, userdata, msg):
         if payload == 'toggle':
             d.get_state(device_mac)
             command = 1 if int(d.get_state(device_mac)) == 0 else 0
-        else:
+        elif payload in ['off', 'on']:
             command = commands[payload]
+        else:
+            logging.warning(f"Command {payload} not valid. Valid commands are 'on', 'off', 'toggle'")
         d.set_state(device_mac, command)
-    # elif 'get_state' in topic:
-    #     d.set_zone_enable(switch_no, payload)
-
-class Devices:
-    def __init__(self):
-        self.device_ips = DEVICE_IPS.split(',')
-        self.devices = {}
-        self.timeout = 3
-
-    def get_device_info(self):
-        """Get information about Tenda Beli device(s)"""
-        for ip in self.device_ips:
-            url = requests.get(f'http://{ip}:5000/getDetail', timeout=self.timeout)
-            info = json.loads(url.text)
-            mac = info['data']['mac'].replace(':','')
-            self.devices[mac] = {}
-            self.devices[mac]['ip_addr'] = ip
-            for item in ['sn', 'model']:
-                self.devices[mac][item] = info['data'][item]
-        
-        print(self.devices)
-
-    def set_state(self, device_mac, state):
-        try:
-            print(f'http://{self.devices[device_mac]["ip_addr"]}:5000/setSta')
-            requests.post(f'http://{self.devices[device_mac]["ip_addr"]}:5000/setSta', data=json.dumps({"status":state}), timeout=self.timeout, headers={'content-type': 'application/json'})
-        except Exception as e:
-            logging.error(f'{e}')
-        d.get_state(device_mac)
-
-    def get_state(self, device_mac):
-        url = requests.get(f'http://{self.devices[device_mac]["ip_addr"]}:5000/getSta')
-        state_text = json.loads(url.text)
-        return state_text['data']['status']
 
 if MQTT_HOST == None:
     logging.error('Please specify the IP address or hostname of your MQTT broker.')
@@ -100,6 +197,11 @@ else:
 
 d = Devices()
 
-mqtt_connect()
-d.get_device_info()
-client.loop_forever()
+if __name__ == '__main__':
+    mqtt_connect()
+    d.get_device_info()
+    for device in d.devices:
+        d.get_state(device)
+    polling_thread = t(target=d.poll_device, daemon=True)
+    polling_thread.start()
+    client.loop_forever()
